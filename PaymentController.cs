b74dd11a3fc0472bfea311858b59f40096d47bce
Sayali1110 <128574626+Sayali1110@users.noreply.github.com>
2025-02-28 19:12:44 +0530

    Add files via upload

---
+++ PaymentController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Project.Models;
+
+namespace Project.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PayemntController : ControllerBase
+    {
+        [HttpGet]
+        public List<PaymentMode> GetModes()
+        {
+            List<PaymentMode> modes = new List<PaymentMode>();
+            using (var db = new p02_efarmingContext())
+            {
+                modes = db.PaymentModes.ToList();
+            }
+            return modes;
+        }
+
+
+        [HttpPost]
+        public PaymentMode SaveMode(PaymentMode mode)
+        {
+            using (var db = new p02_efarmingContext())
+            {
+                db.PaymentModes.Add(mode);
+                db.SaveChanges();
+            }
+            return mode;
+        }
+    }
+}
